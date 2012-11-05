4c3ecd3060ca5f7efe79450be15a671bdcc646d9
Yuko T <yuko.takagi@gmail.com>
2012-11-04 19:52:31 -0800

    cleaning Form class

--- Form_cp.php
+++ Form_cp.php
@@ -29,16 +29,15 @@ class Form extends Module {
         // todo - hardcoded for get & ajax false. Revisit to clean up
         echo '<form action="' .$this->actionUrl .'" method="get" data-ajax="false">';
         echo '<div data-role="fieldcontain" class="ui-field-contain ui-body ui-br">';
- 
-        //echo '<label for="' .$this->selectId .'" class="ui-input-text">' .$this->labelText .'</label>';
-        $this->renderLabel();
-
-        $this->renderFormElements();
-        //echo '<input type="' .$this->inputType .'" name="' .$this->selectId .'" id="' .$this->selectId .'" value="" class="ui-input-text ui-body-c ui-corner-all ui-shadow-inset"/>';
+        echo '<label for="' .$this->selectId .'" class="ui-input-text">' .$this->labelText .'</label>';
+        echo '<input type="' .$this->inputType .'" name="' .$this->selectId .'" id="' .$this->selectId .'" value="" class="ui-input-text ui-body-c ui-corner-all ui-shadow-inset"/>';
         echo '</div>';
 
         echo '<button type="submit" name="submit" value="submit" data-inline="true">' .$this->buttonText .'</button>';
         echo '</form>';
+
+
+
 
 
         /*
@@ -51,15 +50,16 @@ class Form extends Module {
 
             <button type="submit" name="submit" value="submit" data-inline="true">Next</button>
          </form>   
+        
+    
+    
+            <label for="select-choice-0" class="select">Select Card/Bank acct:</label>
+            <select data-inline="true" name="select-choice-0" id="select-choice-0">
+            <option>Visa #1234</option>
+            <option>Amex #5678</option>
+            <option>Citibank #1234</option>
+            </select>
         */
-    }
-
-    public function renderLabel() {
-        echo '<label for="' .$this->selectId .'" class="ui-input-text">' .$this->labelText .'</label>';
-    }
-
-    public function renderFormElements() {
-        echo '<input type="' .$this->inputType .'" name="' .$this->selectId .'" id="' .$this->selectId .'" value="" class="ui-input-text ui-body-c ui-corner-all ui-shadow-inset"/>';
     }
 
 }
