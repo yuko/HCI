75e306c0230a281f7538a1d5056df34824233787
Yuko T <yuko.takagi@gmail.com>
2012-11-06 12:00:43 -0800

    Table class

--- Table.php
+++ Table.php
@@ -16,11 +16,15 @@ class Table extends Module {
         print_r($this->elements[0]);
         echo '<br>';
 
+
+
         echo '<div class="table">';
         for($i = 0; $i < sizeOf($this->elements); $i++) { 
+            echo 'i=' .$i;
+
             echo '<div class="row">';
-            echo '<span class="id">' .$this->elements[$i]['id'] .'</span>';
-            echo '<span class="name">' .$this->elements[$i]['name'] .'</span>';
+            //echo '<span class="id">' .$this->elements[$i]['id'] .'</span>';
+            //echo '<span class="name">' .$this->elements[$i]['name'] .'</span>';
             echo '</div><!-- /row -->';
         }
         echo '</div><!-- /table  -->';
