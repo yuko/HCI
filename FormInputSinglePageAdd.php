633f6f735250fb456aa911914ab5fbbf73cbb021
Yuko T <yuko.takagi@gmail.com>
2012-11-06 15:18:53 -0800

    FormInputSinglePageAdd class

--- FormInputSinglePageAdd.php
+++ FormInputSinglePageAdd.php
@@ -14,14 +14,17 @@ class FormInputSinglePageAdd extends FormInput {
     public function renderButton() {
         $popupId = $this->selectId; //'dummyPopup';
 
-        echo '<a href="#' .$popupId .'" data-role="button" data-inline="true" data-rel="popup">' .$this->buttonText .'</a>';
-
+        //echo '<a href="#' .$popupId .'" data-role="button" data-inline="true" data-rel="popup">' .$this->buttonText .'</a>';
+        echo '<button type="submit" data-rel="popup">' .$this->buttonText .'</button>';
+ 
         echo '<div data-role="popup" id="'. $popupId .'" style="min-width:350px;">';
         echo '<div data-role="header" data-theme="a" class="ui-corner-top">';
         echo '<h3>Added New</h3>';
         echo '</div><!-- /header -->';
         echo '<div data-role="content">';
 
+        echo '<p>Card number:</p>';
+        echo '<p>Card name:</p>';
         echo '<p>Confirmed!</p>';
 
         echo '<a href="#" data-role="button" data-inline="true" data-rel="back" data-theme="c">OK</a>';
