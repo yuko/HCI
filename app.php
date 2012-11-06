64a20205ce53b09c368a49ee1465f0d13689abbe
Yuko T <yuko.takagi@gmail.com>
2012-11-06 13:06:19 -0800

    Table class

--- app.php
+++ app.php
@@ -50,7 +50,7 @@
     $pages['settings-cards-edit'] = new Page('settings-cards-edit', 'Settings > Cards > Add', $nav_settings); 
     $pages['settings-categories'] = new Page('settings-categories', 'Settings > Categories', $nav_settings); 
     $pages['settings-categories-add'] = new Page('settings-categories-add', 'Settings > Categories > Add', $nav_settings); 
-    $pages['settings-categories-edit'] = new Page('settings-categories-edit', 'Settings > Categories > Edit', $nav_settings); 
+    $pages['dummy'] = new Page('dummy', 'Settings > Categories > Edit', $nav_settings); 
     $pages['settings-preferences'] = new Page('settings-preferences', 'Settings > Preferences', $nav_settings); 
 
     // set up pages
