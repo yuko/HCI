3e300ea87a06ab8d32135fbba3bdea4b22b41f49
Yuko T <yuko.takagi@gmail.com>
2012-11-06 12:28:53 -0800

    Table class

--- Table.php
+++ Table.php
@@ -3,19 +3,28 @@
 class Table extends Module {
 
     protected $elements = array();
+    protected $showId;
 
-    function __construct($e) {
+    function __construct($e, $s = false) {
         parent::__construct();
         $this->elements = $e;
+        $this->showId = $s;
     }
 
     public function render() {
         echo '<div class="table">';
-        for($i = 0; $i < sizeOf($this->elements); $i++) { 
+        for($i = 0; $i < sizeOf($this->elements); $i++) {
             echo '<div class="ui-grid-a">';
-            echo '<div class="ui-block-a">' .$this->elements[$i]->getId() .'</div>';
-            echo '<div class="ui-block-b">' .$this->elements[$i]->getName() .'</div>';
-            echo '</div><!-- /row -->';
+                
+            if($this->showId) { 
+                echo '<div class="ui-block-a">' .$this->elements[$i]->getId() .'</div>';
+                echo '<div class="ui-block-b">';
+            } else {   
+                echo '<div class="ui-block-a">';
+            }
+            echo $this->elements[$i]->getName() .'</div>';
+           
+            echo '</div><!-- /ui-grid-a -->';
         }
         echo '</div><!-- /table  -->';
     }
