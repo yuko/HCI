6cfea8a87dcc686acf07985948341788bc5a4501
Yuko T <yuko.takagi@gmail.com>
2012-11-06 18:26:36 -0800

    TransactionTable class

--- Controller.php
+++ Controller.php
@@ -65,7 +65,7 @@ class Controller {
 
     public function getCategoryNameForId($cateId) {
         if(empty($this->ctl_categories)) return 'null yanke';
-        else return sizeOf($this->ctl_categories);
+        else $this->ctl_categories->getId();
 
         for($i = 0; $i < sizeOf($this->ctl_categories); $i++) {
             if($this->ctl_categories[$i]->getCategoryId() == $cardId ) {
